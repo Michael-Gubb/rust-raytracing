dd27543b45e0c4aea17907d208024bc99ddb577b
Michael Gubb <tmgubb@gmail.com>
2024-10-08 17:38:35 +1300

    Add uuid filename creation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use image::{RgbImage, ImageBuffer, Rgb};
+use image::{RgbImage, ImageBuffer, Rgb, ImageError};
+use uuid::Uuid;
+
+fn save_image(buffer:RgbImage)->Result<String,ImageError>{
+    let uuid = Uuid::now_v7();
+    let mut file_name = uuid.to_string();
+    file_name.push_str(".png");
+    buffer.save(&file_name).map(|_|file_name)
+}
+
 
 fn main() {
 
@@ -6,20 +15,20 @@ fn main() {
     const IMAGE_HEIGHT: u32 = 256;
 
     let mut buffer: RgbImage = ImageBuffer::new(IMAGE_WIDTH, IMAGE_HEIGHT);
-
-
-
     for (x, y, pixel) in buffer.enumerate_pixels_mut(){
         let r = x as f64 / (IMAGE_WIDTH-1) as f64;
         let g = y as f64 / (IMAGE_HEIGHT-1) as f64;
         let b = 0.25;
 
-        let ir = (255.999 * r) as u8;
-        let ig = (255.999 * g) as u8;
-        let ib = (255.999 * b) as u8;
+        let ir = (u8::max_value() as f64 * r) as u8;
+        let ig = (u8::max_value() as f64 * g) as u8;
+        let ib = (u8::max_value() as f64 * b) as u8;
 
         *pixel = Rgb([ir, ig, ib]);
     }
 
-    buffer.save("image.png").unwrap();
+    match save_image(buffer){
+        Ok(file_name)=>println!("Printed to: {file_name}"),
+        Err(_)=>eprintln!("Error")
+    }
 }
