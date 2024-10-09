b1bb019c5c1601d953abec77c4aa9978af5e49bd
Michael Gubb <tmgubb@gmail.com>
2024-10-10 11:17:12 +1300

    Add mult and bugfix

--- src/vector.rs
+++ src/vector.rs
@@ -1,14 +1,15 @@
 use core::fmt;
-use std::ops::{Add, Neg, Sub};
+use std::ops::{Add, Mul, Neg, Sub};
+
+/// Any value below this is considered zero for division purposes.
+const EQUIV_ZERO: f64 = 0.0000001;
 #[derive(Debug, Clone, Copy, Default, PartialEq, PartialOrd)]
+
 pub struct Vector3 {
     x: f64,
     y: f64,
     z: f64,
 }
-
-/// Any value below this is considered zero for division purposes.
-const EQUIV_ZERO: f64 = 0.0;
 
 impl Vector3 {
     pub fn new(x: f64, y: f64, z: f64) -> Self {
@@ -34,7 +35,7 @@ impl Vector3 {
     // Returns the length of the vector, None if equivalent to zero
     pub fn length(&self) -> Option<f64> {
         let length = self.unchecked_length();
-        if length <= EQUIV_ZERO {
+        if length > EQUIV_ZERO {
             return Some(length);
         }
         None
@@ -63,19 +64,16 @@ impl From<(f64, f64, f64)> for Vector3 {
         Vector3::new(value.0, value.1, value.2)
     }
 }
-
 impl From<Vector3> for (f64, f64, f64) {
     fn from(value: Vector3) -> Self {
         (value.x, value.y, value.z)
     }
 }
-
 impl From<[f64; 3]> for Vector3 {
     fn from(value: [f64; 3]) -> Self {
         Vector3::new(value[0], value[1], value[2])
     }
 }
-
 impl From<Vector3> for [f64; 3] {
     fn from(value: Vector3) -> Self {
         [value.x, value.y, value.z]
@@ -92,7 +90,6 @@ impl Add for Vector3 {
         }
     }
 }
-
 impl Sub for Vector3 {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self::Output {
@@ -110,6 +107,27 @@ impl Neg for Vector3 {
             x: -self.x,
             y: -self.y,
             z: -self.z,
+        }
+    }
+}
+
+impl Mul<f64> for Vector3 {
+    type Output = Self;
+    fn mul(self, rhs: f64) -> Self {
+        Self::Output {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+impl Mul<Vector3> for f64 {
+    type Output = Vector3;
+    fn mul(self, rhs: Vector3) -> Self::Output {
+        Self::Output {
+            x: rhs.x * self,
+            y: rhs.y * self,
+            z: rhs.z * self,
         }
     }
 }
@@ -186,5 +204,17 @@ mod tests {
         let expected_vector = Vector3::new(-3.0 / 5.0, 4.0 / 5.0, 0.0);
         let expected_length = 5.0;
         assert_eq!(Some((expected_vector, expected_length)), normalised_vector);
+    }
+    #[test]
+    fn zero_length_vector() {
+        let zero_length_vector = Vector3::new(0.0, 0.0, 0.0);
+        let length = zero_length_vector.length();
+        assert_eq!(length, None);
+    }
+    #[test]
+    fn very_short_length_vector() {
+        let short_vector = Vector3::new(0.0, 0.0, EQUIV_ZERO);
+        let length = short_vector.length();
+        assert_eq!(length, None);
     }
 }
